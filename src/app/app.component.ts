d77e89f1476948522ffa4ef0aeaabcefc9156663
Marc Rayner <26255479+marcjrayner@users.noreply.github.com>
2020-02-18 12:40:57 +0000

    angular todo list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,56 @@
 import { Component } from '@angular/core';
+import { Todo } from './todo';
 
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+@Component({ // this is known as the decorator
+  selector: 'app-root', // this is the selector in index.html, it injects  our code wherever it sees this selector
+  templateUrl: './app.component.html', // just for this component
+  styleUrls: ['./app.component.css'] // just for this component
 })
 export class AppComponent {
-  title = 'angular-first';
+  title:string = 'Prajeet Crash Course';
+
+  todoValue: string;
+  list: Todo[];
+
+  ngOnInit() {
+    this.list = [];
+    this.todoValue = "";
+  }
+
+  addItem() {
+    if (this.todoValue !== "") {
+      const newItem: Todo = {
+        id: Date.now(),
+        value: this.todoValue,
+        isDone: false
+      };
+      this.list.push(newItem);
+    }
+    this.todoValue = "";
+  }
+
+  deleteItem(id:number) {
+    this.list = this.list.filter(item => item.id !== id);
+  }
+
+
+
+
+
+
+
+
+
+
+
+  // myName:string =  "Marco Pierre Rayner"
+  //
+  // constructor() {
+  //   console.log("constructor called!");
+  //   this.doSomething(this.myName);
+  // }
+  //
+  // doSomething(value:string) string {
+  //   return value + "!"
+  // }
 }
